f685936cf668ac310de3dbee9ca2c6c114a010b2
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-21 16:37:53 +0530

    some more feature added

--- pms_system/hooks.py
+++ pms_system/hooks.py
@@ -17,7 +17,7 @@ fixtures = [
     {
         "doctype": "Property Setter",
         "filters": [
-            ["name", "in", ["Goal-section_break_cycle-hidden"]]
+            ["name", "in", ["Goal-section_break_cycle-hidden","Appraisee-appraisal_template-hidden"]]
         ]
     }
 ]
--- pms_system/public/js/appraisalCycle.js
+++ pms_system/public/js/appraisalCycle.js
@@ -1,3 +1,40 @@
 frappe.ui.form.on("Appraisal Cycle", {
+    refresh(frm) {
+        // run standard refresh first
+        this._super && this._super(frm);
 
+        // override the button after standard buttons are created
+        frm.trigger("override_create_appraisal_button");
+    },
+
+    override_create_appraisal_button(frm) {
+        frm.remove_custom_button("Create Appraisals");
+        frm.page.clear_primary_action();
+
+        let appraisals_created = frm.doc.__onload?.appraisals_created;
+        if (frm.doc.status !== "Completed") {
+            if (appraisals_created) {
+                frm.add_custom_button(__("Create Appraisals"), () => {
+                    frm.trigger("my_custom_create_appraisals");
+                });
+            } else {
+                frm.page.set_primary_action(__("Create Appraisals"), () => {
+                    frm.trigger("my_custom_create_appraisals");
+                });
+            }
+        }
+    },
+
+    my_custom_create_appraisals(frm) {
+        frappe.msgprint(__("Custom Appraisal Creation Triggered"));
+        // frappe.call({
+        //     method: "your_app.your_module.doctype.appraisal_cycle.appraisal_cycle.custom_create_appraisals",
+        //     args: { docname: frm.doc.name },
+        //     freeze: true,
+        //     freeze_message: __("Running Custom Appraisal Creation"),
+        //     callback() {
+        //         frm.reload_doc();
+        //     }
+        // });
+    }
 })
