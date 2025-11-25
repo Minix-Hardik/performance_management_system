d46eb987cbbeee4ed60a9c6560860e1aae349738
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-25 22:38:03 +0530

    repoprts to added

--- pms_system/api/create_appraisal_list.py
+++ pms_system/api/create_appraisal_list.py
@@ -1,26 +1,44 @@
 import frappe
 
+def get_reports_to(employee, level):
+    """
+    level = 1  → Single Level
+    level = 2  → Second Level
+    """
+
+    # Get immediate manager
+    first_manager = frappe.db.get_value("Employee", employee, "reports_to")
+
+    if not first_manager:
+        return None
+
+    if level == 1:
+        return first_manager
+
+    # Get second level manager
+    second_manager = frappe.db.get_value("Employee", first_manager, "reports_to")
+
+    return second_manager if second_manager else first_manager
+
+
 @frappe.whitelist()
 def create_appraisal_list(doc_name):
     doc = frappe.get_doc("Appraisal Cycle", doc_name)
-
-    # get employee rows from Appraisees child table
-    employees = doc.appraisees  # child table fieldname
-    
-    # get template map (if using designation-template mapping)
+    employees = doc.appraisees 
     appraisal_templates = doc.get_appraisal_template_map() if hasattr(doc, "get_appraisal_template_map") else {}
 
     created_appraisals = []
-
+    report_level = 1 if doc.custom_reports_to == "Single Level" else 2
     if employees:
         for d in employees:
-
+            manager = get_reports_to(d.employee, report_level)
             appraisal = frappe.get_doc({
                 "doctype": "Appraisal List",
                 "employee": d.employee,          
                 "appraisal_cycle": doc.name,
                 "start_date": doc.start_date,
-                "end_date": doc.end_date
+                "end_date": doc.end_date,
+                "reports_to":manager
             })
 
             appraisal.insert(ignore_permissions=True)
