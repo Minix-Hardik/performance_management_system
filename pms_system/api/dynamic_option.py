7cce4bcc898b1f34b453e2a94e7e6186ca177ba1
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-25 20:18:09 +0530

    fixed some more

--- pms_system/api/dynamic_option.py
+++
@@ -1,6 +0,0 @@
-@frappe.whitelist()
-def get_options(doc=None, doctype=None, docname=None, fieldname=None):
-    row = frappe.get_doc(doctype, docname)
-        options = row.options or ""
-        opts = [o.strip() for o in options.split(",") if o.strip()]
-        return "\n".join(opts)
