37b66d6174b18944e4e64d746f16b3bd4ed6f392
alik02-laha <alik@mail.hybrowlabs.com>
2025-11-26 18:33:09 +0530

    added new logic to make some field readonly and make some hidden

--- pms_system/pms_system/doctype/appraisal_list/appraisal_list.js
+++ pms_system/pms_system/doctype/appraisal_list/appraisal_list.js
@@ -1,14 +1,107 @@
 // Copyright (c) 2025, Hybrowlabs and contributors
 // For license information, please see license.txt
 
+
+function make_all_readonly(frm) {
+    Object.keys(frm.fields_dict).forEach(f => {
+        frm.set_df_property(f, "read_only", 1);
+    });
+    frm.meta.fields
+        .filter(df => df.fieldtype === "Table")
+        .forEach(table_df => {
+
+            const table = table_df.fieldname;
+            frm.set_df_property(table, "read_only", 1);
+            let child_meta = frappe.get_meta(table_df.options);
+
+            child_meta.fields.forEach(cdf => {
+                frm.fields_dict[table].grid.update_docfield_property(
+                    cdf.fieldname,
+                    "read_only",
+                    1
+                );
+            });
+
+            frm.refresh_field(table);
+        });
+}
+
+
+function make_readonly_except_multi(frm, table_map) {
+    make_all_readonly(frm);
+
+    Object.entries(table_map).forEach(([table, cfg]) => {
+
+        frm.set_df_property(table, "read_only", 0);
+
+        let grid = frm.fields_dict[table].grid;
+        let child_meta = frappe.get_meta(grid.doctype);
+
+        let editable_fields = cfg.readonly || [];
+        let hidden_fields = cfg.hidden || [];
+
+        child_meta.fields.forEach(cdf => {
+            let fname = cdf.fieldname;
+
+            // HIDE only the specified fields
+            if (hidden_fields.includes(fname)) {
+                grid.update_docfield_property(fname, "hidden", 1);
+                grid.update_docfield_property(fname, "read_only", 1);
+                return;
+            }
+
+            // MAKE ONLY THESE FIELDS EDITABLE
+            if (editable_fields.includes(fname)) {
+                grid.update_docfield_property(fname, "read_only", 0);
+                // DO NOT TOUCH hidden property
+                return;
+            }
+            grid.update_docfield_property(fname, "read_only", 1);
+        });
+
+        frm.refresh_field(table);
+    });
+}
+
+
+
 frappe.ui.form.on('Appraisal List', {
     onload: function (frm) {
         set_all_employee_options(frm);
+        make_all_readonly(frm)
+        const user = frappe.session.user;
+        if (frm.doc.workflow_state === "Self Appraisal" && frm.doc.employee_user_id === user) {
+            make_readonly_except_multi(frm, {
+                answer: {
+                    readonly: ["employee_ans_in_option", "employee_ans_in_discriptive"],
+                    hidden: []
+
+                },
+                competency: {
+                    readonly: ["employee_rating_number", "employee_description"],
+                    hidden: ["manager_rating", "manager_description"]
+                }
+            });
+        }
     },
     refresh: function (frm) {
         set_all_employee_options(frm);
-        $(frm.$wrapper).find('.form-dashboard-section.form-stats').hide();
-        $(frm.$wrapper).find('.form-dashboard-section.form-links').hide();
+        const user = frappe.session.user;
+        make_all_readonly(frm);
+        if (frm.doc.workflow_state === "Self Appraisal" && frm.doc.employee_user_id === user) {
+            make_readonly_except_multi(frm, {
+                answer: {
+                    readonly: ["employee_ans_in_option", "employee_ans_in_discriptive"],
+                    hidden: []
+
+                },
+                competency: {
+                    readonly: ["employee_rating_number", "employee_description"],
+                    hidden: ["manager_rating", "manager_description"]
+                }
+            });
+        }
+
     }
 });
 
