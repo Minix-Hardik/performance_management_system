def652bf73f1c3aee7645071b5fd537b1d3b20bd
alik02-laha <alik@mail.hybrowlabs.com>
2025-12-08 17:06:21 +0530

    skill is also done

--- pms_system/pms_system/doctype/skill_set/skill_set.js
+++ pms_system/pms_system/doctype/skill_set/skill_set.js
@@ -8,7 +8,8 @@ frappe.ui.form.on("Skill Set", {
             "read_only",
             !frm.is_new() ? 1 : 0
         );
-        if (!frm.is_new()) {
+        current_user = window?.frappe?.session?.user;
+        if (!frm.is_new() && (frm.doc.reporting_manager_user_id === current_user || frm.doc.employee_user_id === current_user)) {
 
             frm.add_custom_button("Skill Level Up", function () {
                 open_skill_popup(frm, "up");
@@ -22,7 +23,14 @@ frappe.ui.form.on("Skill Set", {
     }
 });
 
-function open_skill_popup(frm, action) {
+async function open_skill_popup(frm, action) {
+    current_user = window?.frappe?.session?.user;
+    const pms_number = await frappe.db.get_single_value("PMS Setting", "skill_max_level")
+    console.log("PMS Number:", pms_number);
+    if (frm.doc.skill_level >= pms_number && action === "up") {
+        frappe.msgprint(`Skill Level cannot be increased beyond ${pms_number}.`);
+        return;
+    }
     const dialog = new frappe.ui.Dialog({
         title: "Update Skill Level",
         fields: [
@@ -30,7 +38,7 @@ function open_skill_popup(frm, action) {
                 fieldname: "remarks",
                 label: "Remarks",
                 fieldtype: "Small Text",
-                reqd: 1
+                reqd: frm.doc.reporting_manager_user_id === current_user ? 1 : 0
             }
         ],
         primary_action_label: "Submit",
@@ -47,6 +55,7 @@ function open_skill_popup(frm, action) {
             }
 
             frm.set_value("skill_level", new_value);
+            frm.set_value("remarks", values.remarks || "");
             frm.save();
 
             dialog.hide();
--- pms_system/pms_system/doctype/skill_set/skill_set.py
+++ pms_system/pms_system/doctype/skill_set/skill_set.py
@@ -10,5 +10,20 @@ class SkillSet(Document):
 		employee = frappe.get_doc("Employee", self.employee)
 		self.reporting_manager = employee.reports_to
 		self.save()
+	def save_skill_level_log(self):
+		old_skill_data = self.get_doc_before_save() 
+		if old_skill_data and old_skill_data.skill_level != self.skill_level:
+			self.append("skill_level_change_log", {
+				"old_level": old_skill_data.skill_level,
+				"new_level": self.skill_level,
+				"changed_by": frappe.session.user,
+				"remarks": self.remarks,
+				"updated_at": frappe.utils.now(),
+				"changed_by": frappe.session.user
+			})
+
+
+	def before_save(self):
+		self.save_skill_level_log()
 	def after_insert(self):
 		self.set_reporting_manager()
