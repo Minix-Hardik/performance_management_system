a3571b17ceffaed932b92fe1c4340c89715f9b56
Pithiya-Hardik <pithiyahardik18@gmail.com>
2026-06-20 16:13:45 +0530

    fix the click the rating error

--- pms_system/__init__.py
+++ pms_system/__init__.py
@@ -1,3 +1,15 @@
 __version__ = "0.0.1"
 import pms_system.overrides.goal
 import pms_system.overrides.appraisal
+
+# Monkeypatch frappe.desk.search.get_link_title to fix OperationalError when title_field is not set
+import frappe.desk.search
+
+def custom_get_link_title(doctype, docname):
+	meta = frappe.get_meta(doctype)
+	if meta.show_title_field_in_link and meta.title_field:
+		return frappe.db.get_value(doctype, docname, meta.title_field)
+	return docname
+
+frappe.desk.search.get_link_title = custom_get_link_title
+
