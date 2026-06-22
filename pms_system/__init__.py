5ae6b8caeeb7f6d50896f0fef16bdd3d4dbb21b6
Pithiya-Hardik <pithiyahardik18@gmail.com>
2026-06-22 15:00:39 +0530

    fix the function not called

--- pms_system/__init__.py
+++ pms_system/__init__.py
@@ -3,8 +3,10 @@ import pms_system.overrides.goal
 import pms_system.overrides.appraisal
 
 # Monkeypatch frappe.desk.search.get_link_title to fix OperationalError when title_field is not set
+import frappe
 import frappe.desk.search
 
+@frappe.whitelist()
 def custom_get_link_title(doctype, docname):
 	meta = frappe.get_meta(doctype)
 	if meta.show_title_field_in_link and meta.title_field:
