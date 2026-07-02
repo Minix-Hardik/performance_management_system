5b41b78443ab472e26a7580cdfd8d5dfeecf9af5
Pithiya-Hardik <pithiyahardik18@gmail.com>
2026-07-02 14:58:19 +0530

    add button to get tocken

--- pms_system/pms_system/doctype/user_jira_connection/user_jira_connection.js
+++ pms_system/pms_system/doctype/user_jira_connection/user_jira_connection.js
@@ -3,8 +3,16 @@
 
 frappe.ui.form.on("User Jira Connection", {
 	onload(frm) {
-		if (frm.is_new() && !frm.doc.user) {
-			frm.set_value("user", frappe.session.user);
+		if (frm.is_new()) {
+			if (!frm.doc.user) {
+				frm.set_value("user", frappe.session.user);
+			}
+			if (frappe.session.user !== 'Administrator' && !frm.doc.jira_email) {
+				frm.set_value("jira_email", frappe.session.user);
+			}
 		}
+	},
+	generate_token_btn(frm) {
+		window.open('https://id.atlassian.com/manage-profile/security/api-tokens', '_blank');
 	}
 });
